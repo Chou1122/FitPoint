f85fa445fa91589c474a1a7bd9b4d13f56c5a963
MinhPH <minhph@sanancorp.com>
2025-03-12 17:30:29 +0700

    add vertical jump

--- src/screens/SportRecording/get-process-video-from-id.ts
+++ src/screens/SportRecording/get-process-video-from-id.ts
@@ -2,6 +2,8 @@ export const GetProcessVideoUrlFromId = (sportId: number | string) => {
   switch (sportId) {
     case 1:
       return 'process-push-up';
+    case 2: 
+      return 'process-vertical-jump';
     case 3:
       return 'process-plank';
     default:
